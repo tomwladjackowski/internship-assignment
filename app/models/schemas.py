1a21d4844b0a89e947414a4a49b5cd8c14b1206b
Tomasz Jackowski <tomwladjackowski@gmail.com>
2025-01-20 14:27:20 +0100

    added pydantic models for input and output

---
+++ app/models/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, List
+
+class DistanceRange(BaseModel) :
+    min: int
+    max: int
+    a: int
+    b: int
+
+class Venue(BaseModel) :
+    venue_coordinates: List[int]
+    min_cart_value: int
+    base_price: int
+    distance_ranges: List[DistanceRange]
+
+class DeliveryInputModel(BaseModel) :
+    venue_slug: str
+    cart_value: int
+    user_lat: float
+    user_lon: float
+
+class DeliveryFeeModel(BaseModel) :
+    fee: int 
+    distance: int
+class DeliveryOutputModel(BaseModel) :
+    total_price: int
+    small_order_surcharge: int
+    cart_value: int
+    delivery: DeliveryFeeModel
+
