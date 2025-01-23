4b80bb01e31ce0277d7e031bdd1ff9e5d58b03df
Tomasz Jackowski <tomwladjackowski@gmail.com>
2025-01-23 15:06:44 +0100

    changed imports to use Depends instead of Annotated

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Query
-from typing import Annotated
+from fastapi import APIRouter, Depends
 from app.models.schemas import DeliveryOutputModel, DeliveryInputModel
 from app.services.deliver_fee_service import calculate_delivery_fee
 
@@ -10,5 +9,5 @@ def main_route():
     return {"message": "Server is running"}
 
 @router.get("/api/v1/delivery-order-price", response_model = DeliveryOutputModel)
-async def get_delivery_price(query_params: Annotated[DeliveryInputModel, Query()]) :
+def get_delivery_price(query_params: DeliveryInputModel = Depends()) -> DeliveryOutputModel :
     return calculate_delivery_fee(query_params.venue_slug, query_params.cart_value, query_params.user_lat, query_params.user_lon)
