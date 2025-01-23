aa7cd4e7a9b87e4836bf083b3b5a4ab95bff3f55
Tomasz Jackowski <tomwladjackowski@gmail.com>
2025-01-23 15:07:52 +0100

    imported an exception handler for RequestValidation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from app.api.routes import router
+from app.exceptions import validation_exception_handler
 
 app = FastAPI()
+
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 app.include_router(router)
